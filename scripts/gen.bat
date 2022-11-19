cfa0d11cc4da62117be98971d106accbec32ab39
Shawn Bragdon <shawnjb.contact@gmail.com>
2022-11-18 20:51:24 -0800

    Update scripts.

--- scripts/gen.bat
+++
@@ -1,4 +0,0 @@
-REM | Generates Lua documentation for the project.
-
-ldoc.lua "./algorithms" -d "./site/docs/algorithms" -o "index" -f "markdown"
-ldoc.lua "./structures" -d "./site/docs/structures" -o "index" -f "markdown"
